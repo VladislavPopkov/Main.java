a342de92783c7e04953bde1af7c33e271078b2a1
VladislavPopkov <105574941+VladislavPopkov@users.noreply.github.com>
2022-07-26 16:17:23 +1100

    ready2

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -12,19 +12,19 @@ public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
        UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
-//       userDaoJDBC.removeUserById(3);
-//        User one = new User("Tomas", "Shelbi",  (byte)44);
-//        User two = new User("Richard", "Portman",  (byte)37);
-//        User three = new User("Leonardo", "Dicaprio",  (byte)49);
-//        User four = new User("Eva", "Green",  (byte)28);
-//        userDaoJDBC.saveUser(one.getName(), one.getLastName(), one.getAge());
-//        userDaoJDBC.saveUser(two.getName(), two.getLastName(), two.getAge());
-//        userDaoJDBC.saveUser(three.getName(), three.getLastName(), three.getAge());
-//        userDaoJDBC.saveUser(four.getName(), four.getLastName(), four.getAge());
-//        userDaoJDBC.createUsersTable();
-//        System.out.println(userDaoJDBC.getAllUsers());
-//      userDaoJDBC.cleanUsersTable();
-//       userDaoJDBC.dropUsersTable();
+       userDaoJDBC.removeUserById(3);
+        User one = new User("Tomas", "Shelbi",  (byte)44);
+        User two = new User("Richard", "Portman",  (byte)37);
+        User three = new User("Leonardo", "Dicaprio",  (byte)49);
+        User four = new User("Eva", "Green",  (byte)28);
+        userDaoJDBC.saveUser(one.getName(), one.getLastName(), one.getAge());
+        userDaoJDBC.saveUser(two.getName(), two.getLastName(), two.getAge());
+        userDaoJDBC.saveUser(three.getName(), three.getLastName(), three.getAge());
+        userDaoJDBC.saveUser(four.getName(), four.getLastName(), four.getAge());
+        userDaoJDBC.createUsersTable();
+        System.out.println(userDaoJDBC.getAllUsers());
+      userDaoJDBC.cleanUsersTable();
+       userDaoJDBC.dropUsersTable();
 
 
 
